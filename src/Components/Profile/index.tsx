05e7473f6994d4fa85b8b222ce1ad80b466c199d
PooyaNaghavi <naghavi_pooya@yahoo.com>
2019-05-05 09:59:08 +0430

    addSkill null bug fixed

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,126 +1,122 @@
-import React, { Component } from 'react';
-import './style.css';
-import Footer from '../Utils/Footer';
-import Header from '../Utils/Header';
+import React, { Component } from "react";
+import "./style.css";
+import Footer from "../Utils/Footer";
+import Header from "../Utils/Header";
 
-import UserInfo from './UserInfo';
-import axios from 'axios';
-import UserSkills from './UserSkills';
+import UserInfo from "./UserInfo";
+import axios from "axios";
+import UserSkills from "./UserSkills";
 
 export interface ProfileProps {
-    location: any,
+  location: any;
 }
 
 export interface ProfileState {
-    userId: string | null,
+  userId: string | null;
 
-    firstName: string,
-    jobTitle: string,
-    lastName: string,
-    profilePictureURL: string,
+  firstName: string;
+  jobTitle: string;
+  lastName: string;
+  profilePictureURL: string;
 
-    bio: string,
+  bio: string;
 
-    skills: Array<any>,
-    allSkills: Array<any>,
+  skills: Array<any>;
+  allSkills: Array<any>;
 
-    error: boolean,
+  error: boolean;
 }
 
 class Profile extends React.Component<ProfileProps, ProfileState> {
-    constructor(props: ProfileProps) {
-        super(props);
-        this.state = {
-            userId: null,
-            bio: '',
-            firstName: '',
-            jobTitle: '',
-            lastName: '',
-            profilePictureURL: '',
-            skills: [],
-            allSkills: [],
-            error: false,
-        };
-    }
+  constructor(props: ProfileProps) {
+    super(props);
+    this.state = {
+      userId: null,
+      bio: "",
+      firstName: "",
+      jobTitle: "",
+      lastName: "",
+      profilePictureURL: "",
+      skills: [],
+      allSkills: [],
+      error: false
+    };
+  }
 
-    componentDidMount() {
-        const queryParams = new URLSearchParams(this.props.location.search);
-        const userId = queryParams.get('id')
+  componentDidMount() {
+    const queryParams = new URLSearchParams(this.props.location.search);
+    const userId = queryParams.get("id");
+    this.setState({
+      userId
+    });
+
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/user?id=${userId}`, {
+        headers: {
+          "content-type": "application/json; charset=utf-8"
+        }
+      })
+      .then((response: any) => {
         this.setState({
-            userId,
-        })
+          firstName: response.data.user.firstName,
+          jobTitle: response.data.user.jobTitle,
+          lastName: response.data.user.lastName,
+          profilePictureURL: response.data.user.profilePictureURL,
 
-        axios.get(
-            `${process.env.REACT_APP_BASE_URL}/user?id=${userId}`,
-            {
-                headers: {
-                    "content-type": "application/json; charset=utf-8"
-                }
-            }
-        ).then((response: any) => {
+          bio: response.data.user.bio,
 
+          skills: response.data.user.skills,
+          allSkills: response.data.allSkills,
 
-            this.setState({
-                firstName: response.data.user.firstName,
-                jobTitle: response.data.user.jobTitle,
-                lastName: response.data.user.lastName,
-                profilePictureURL: response.data.user.profilePictureURL,
-
-                bio: response.data.user.bio,
-
-                skills: response.data.user.skills,
-                allSkills: response.data.allSkills,
-
-                error: false,
-            })
-        }).catch((err: any) => {
-            console.error(err)
-            this.setState({
-                error: true
-            })
-
+          error: false
         });
-    }
+      })
+      .catch((err: any) => {
+        console.error(err);
+        this.setState({
+          error: true
+        });
+      });
+  }
 
-
-    render() {
-        const {
-            firstName,
-            lastName,
-            userId,
-            jobTitle,
-            profilePictureURL,
-            bio,
-            skills,
-            allSkills,
-        } = this.state
-        return (
-            <div>
-                <Header />
-                <div className="row profile-blue-background"></div>
-                <div className="container body-container">
-                    <UserInfo
-                        firstName={firstName}
-                        lastName={lastName}
-                        id={userId}
-                        jobTitle={jobTitle}
-                        profilePictureURL={profilePictureURL}
-                    />
-                    <div className="row">
-                        <div className="col-1 dummy"></div>
-                        <div className="col-10">
-                            <div id="description">
-                                <p id="bio-text">{bio}</p>
-                            </div>
-                        </div>
-                        <div className="col-1 dummy"></div>
-                    </div>
-                    <UserSkills skills={skills} allSkills={allSkills} userId={userId} />
-                </div>
-                <Footer />
+  render() {
+    const {
+      firstName,
+      lastName,
+      userId,
+      jobTitle,
+      profilePictureURL,
+      bio,
+      skills,
+      allSkills
+    } = this.state;
+    return (
+      <div>
+        <Header />
+        <div className="row profile-blue-background" />
+        <div className="container body-container">
+          <UserInfo
+            firstName={firstName}
+            lastName={lastName}
+            id={userId}
+            jobTitle={jobTitle}
+            profilePictureURL={profilePictureURL}
+          />
+          <div className="row">
+            <div className="col-1 dummy" />
+            <div className="col-10">
+              <div id="description">
+                <p id="bio-text">{bio}</p>
+              </div>
             </div>
-        );
-    }
+            <div className="col-1 dummy" />
+          </div>
+          <UserSkills skills={skills} allSkills={allSkills} userId={userId} />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Profile;
--- src/Components/Utils/Skill/index.tsx
+++ src/Components/Utils/Skill/index.tsx
@@ -1,118 +1,122 @@
-import React, { Component } from 'react';
-import './style.css'
-import axios from 'axios';
-
+import React, { Component } from "react";
+import "./style.css";
+import axios from "axios";
 
 export interface SkillProps {
-    name: string,
-    point: number,
-    selfSkill: boolean,
-    endorsed: boolean,
-    disabled: boolean,
-    userToEndorse?: string | null,
+  name: string;
+  point: number;
+  selfSkill: boolean;
+  endorsed: boolean;
+  disabled: boolean;
+  userToEndorse?: string | null;
 }
 
 export interface SkillState {
-    name: string,
-    point: number,
-    endorsed: boolean,
-    disabled: boolean,
-    deleted: boolean,
-    error: boolean,
+  name: string;
+  point: number;
+  endorsed: boolean;
+  disabled: boolean;
+  deleted: boolean;
+  error: boolean;
 }
 
 class Skill extends React.Component<SkillProps, SkillState> {
-    constructor(props: SkillProps) {
-        super(props);
-        this.state = {
-            name: '',
-            point: this.props.point,
-            endorsed: this.props.endorsed,
-            disabled: this.props.disabled,
-            deleted: false,
-            error: false,
-        };
+  constructor(props: SkillProps) {
+    super(props);
+    this.state = {
+      name: "",
+      point: this.props.point,
+      endorsed: this.props.endorsed,
+      disabled: this.props.disabled,
+      deleted: false,
+      error: false
+    };
+  }
+  render() {
+    const { name, selfSkill } = this.props;
+    const { endorsed, point, disabled, deleted } = this.state;
+    return (
+      <div className={`skill ${deleted ? "hidden" : ""}`}>
+        <span className="skill-text">{name}</span>
+        <button
+          className={`skill-button ${endorsed ? "endorsed" : "not-endorsed"} ${
+            selfSkill ? "deleted" : ""
+          }`}
+          disabled={disabled}
+          onClick={e => this.handleOnClick(e)}
+        >
+          <span>{point}</span>
+        </button>
+      </div>
+    );
+  }
+  handleOnClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    if (this.props.disabled) {
+      return;
+    } else if (this.props.selfSkill) {
+      this.handleRemoveSkill(e);
+    } else if (!this.props.endorsed) {
+      this.handleEndorseSkill(e);
     }
-    render() {
-        const {
-            name,
-            selfSkill,
-        } = this.props
-        const { endorsed, point, disabled, deleted } = this.state
-        return (
-            <div className={`skill ${deleted ? 'hidden' : ''}`}>
-                <span className="skill-text">{name}</span>
-                <button
-                    className={`skill-button ${endorsed ? 'endorsed' : 'not-endorsed'} ${selfSkill ? 'deleted' : ''}`}
-                    disabled={disabled}
-                    onClick={e => this.handleOnClick(e)} >
-                    <span>{point}</span>
-                </button>
-            </div>
-        );
-    }
-    handleOnClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-        if (this.props.disabled) {
-            return
-        } else if (this.props.selfSkill) {
-            this.handleRemoveSkill(e)
-        } else if (!this.props.endorsed) {
-            this.handleEndorseSkill(e)
+  }
+
+  handleEndorseSkill(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): any {
+    e.preventDefault();
+    axios
+      .put(
+        `${process.env.REACT_APP_BASE_URL}/user/skill/${this.props.name}?id=${
+          this.props.userToEndorse
+        }`,
+        // TODO: Change all these '1's to user from Cookies
+        {
+          headers: {
+            "content-type": "application/json; charset=utf-8"
+          }
         }
-    }
-
-    handleEndorseSkill(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): any {
-        e.preventDefault()
-        axios.put(
-            `${process.env.REACT_APP_BASE_URL}/user/skill/${this.props.name}?id=${this.props.userToEndorse}`,
-            // TODO: Change all these '1's to user from Cookies
-            {
-                headers: {
-                    "content-type": "application/json; charset=utf-8"
-                }
-            }
-        ).then((response: any) => {
-            this.setState({
-                endorsed: true,
-                point: this.state.point + 1,
-                disabled: true,
-                error: false,
-            })
-
-        }).catch((err: any) => {
-            console.error(err)
-            this.setState({
-                error: true
-            })
+      )
+      .then((response: any) => {
+        this.setState({
+          endorsed: true,
+          point: this.state.point + 1,
+          disabled: true,
+          error: false
         });
-
-    }
-
-    handleRemoveSkill(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): any {
-        e.preventDefault()
-        if (!confirm('آیا مطمئن هستید؟')) return
-        axios.delete(
-            `${process.env.REACT_APP_BASE_URL}/user/skill/${this.props.name}`,
-            // TODO: Change all these '1's to user from Cookies
-            {
-                headers: {
-                    "content-type": "application/json; charset=utf-8"
-                }
-            }
-        ).then((response: any) => {
-            this.setState({
-                deleted: true,
-                error: false,
-            })
-        }).catch((err: any) => {
-            console.error(err)
-            alert('درخواست شما با خطا مواجه شد، لطفا دوباره تلاش کنید.')
-            this.setState({
-                error: true
-            })
+      })
+      .catch((err: any) => {
+        console.error(err);
+        this.setState({
+          error: true
         });
-    }
+      });
+  }
 
+  handleRemoveSkill(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): any {
+    e.preventDefault();
+    if (!confirm("آیا مطمئن هستید؟")) return;
+    axios
+      .delete(
+        `${process.env.REACT_APP_BASE_URL}/user/skill/${this.props.name}`,
+        // TODO: Change all these '1's to user from Cookies
+        {
+          headers: {
+            "content-type": "application/json; charset=utf-8"
+          }
+        }
+      )
+      .then((response: any) => {
+        this.setState({
+          deleted: true,
+          error: false
+        });
+      })
+      .catch((err: any) => {
+        console.error(err);
+        alert("درخواست شما با خطا مواجه شد، لطفا دوباره تلاش کنید.");
+        this.setState({
+          error: true
+        });
+      });
+  }
 }
 
 export default Skill;
